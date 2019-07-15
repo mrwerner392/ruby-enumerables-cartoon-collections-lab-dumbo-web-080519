2cb74e309e6a3fcd43be6791dc76545cf009fa82
Matt Werner <mrwerner392@gmail.com>
2019-07-15 01:24:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# print out array elements in a numbered list
+def roll_call_dwarves(array)
+  array.each_with_index{ |elem,i| puts "#{i+1}. #{elem}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# capitalize and add exclamation to each element in an array
+def summon_captain_planet(array)
+  new_array = array.map{ |elem| "#{elem.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# check if any element is longer than 4 characters
+def long_planeteer_calls(array)
+  array.any?{ |elem| elem.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# find the first element that is a type of cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{ |elem| cheese_types.include?(elem) }
 end
