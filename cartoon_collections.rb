4d230c4d31c9c3269aa730e129d4f8a40c22726f
Matt Werner <mrwerner392@gmail.com>
2019-07-15 01:20:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 # check if any element is longer than 4 characters
-def long_planeteer_calls(array))
+def long_planeteer_calls(array)
   array.any?{ |elem| elem.length > 4 }
 end
 
